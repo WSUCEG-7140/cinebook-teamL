e3fbf97f60b7cbd8f4dc5379b3ae02328f7e9f69
Nallapati25 <133615098+Nallapati25@users.noreply.github.com>
2023-07-11 12:11:44 -0500

    Update  MoviesDisplay.js for see more

--- Frontend/src/Components/MoviesDisplay.js
+++ Frontend/src/Components/MoviesDisplay.js
@@ -10,33 +10,47 @@ export default function MoviesDisplay() {
         const response=await fetch('http://localhost:8080/movies/getByStatus?status=TRENDING');
         const json=await response.json();
         setTendingMovieData(json)
+        setTendingMovieData(trendingMoviesData.slice(0,10))
         const response2=await fetch('http://localhost:8080/movies/getByStatus?status=COMMING_SOON');
         const json2=await response2.json();
         setComingMovieData(json2)
+        setComingMovieData(comingMoviesData.slice(0,10))
     }
     
     useEffect(() => {
         fetchData()
       }, [])
+    const handleSeeMore=(e)=>{
+        if(e.target.id=="trending){
+           const response=await fetch('http://localhost:8080/movies/getByStatus?status=TRENDING');
+            const json=await response.json();
+            setTendingMovieData(json)
+        }else if(e.target.id=="upcoming){
+                 const response2=await fetch('http://localhost:8080/movies/getByStatus?status=COMMING_SOON');
+                const json2=await response2.json();
+                setComingMovieData(json2)
+        }     
+    }
     return (
         <div>
             <div class="MovieDisplayHeaderContainer">
                 <h1>Trending Movies</h1>
             </div>
             <div class="MovieDisplayContainer">
-                {trendingMoviesData.slice(0,10).map((d) => (
+                {trendingMoviesData.map((d) => (
                     <Card data={d} />
                 ))}
-                <p className="h4style">See More</p>
+                <p className="h4style" id="trending" onClick={handleSeeMore}>See More</p>
             </div>
             <div class="MovieDisplayHeaderContainer">
                 <h1>Coming Soon</h1>
             </div>
             <div class="MovieDisplayContainer">
                 {
-                comingMoviesData.slice(0,10).map((d) => (
+                comingMoviesData.map((d) => (
                     <Card data={d} />
                 ))}
+                <p className="h4style" id="upcoming" onClick={handleSeeMore}>See More</p>
             </div>
         </div>
     );
